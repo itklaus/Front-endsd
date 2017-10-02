f6520f4d7c6eaa2dba6257222c0c7d1df720ec3d
itklaus <it.thomas@yandex.ru>
2017-10-02 21:48:34 +0300

    Add a browser-sync and gulp-rename

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,21 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
+const browserSync = require('browser-sync').create();
+const rename = require('gulp-rename');
 
 gulp.task('es6', () => {
-  return gulp.src('src/master.js')
+  return gulp.src('src/js/es6.js')
     .pipe(babel())
-    .pipe(gulp.dest('dist'));
+    .pipe(rename('js/master.js'))
+    .pipe(gulp.dest('src'));
 });
 
 gulp.task('default', ['es6'], () => {
-  gulp.watch('src/master.js', ['es6'])
+  browserSync.init({
+    server: {
+      baseDir: 'src'
+    }
+  });
+  gulp.watch('src/**/*.js', ['es6']);
+  gulp.watch(['src/index.html', 'src/**/*.js']).on('change', browserSync.reload);
 });
