1b947eb19199221be9d6aa1254ecad4511371226
itklaus <it.thomas@yandex.ru>
2017-10-05 00:22:41 +0300

    Add a pug

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,31 @@ const gulp = require('gulp');
 const babel = require('gulp-babel');
 const browserSync = require('browser-sync').create();
 const rename = require('gulp-rename');
+const pug = require('gulp-pug');
 
 gulp.task('es6', () => {
   return gulp.src('src/js/es6.js')
     .pipe(babel())
-    .pipe(rename('js/master.js'))
+    .pipe(rename('../dest/js/master.js'))
     .pipe(gulp.dest('src'));
 });
 
-gulp.task('default', ['es6'], () => {
+gulp.task('pug', () => {
+  gulp.src('src/*.pug')
+  .pipe(pug({
+    pretty: true
+  }))
+  .pipe(gulp.dest('dest/'))
+});
+
+gulp.task('default', ['es6', 'pug'], () => {
   browserSync.init({
     server: {
-      baseDir: 'src'
-    }
+      baseDir: 'dest'
+    },
+    notify: false
   });
   gulp.watch('src/**/*.js', ['es6']);
-  gulp.watch(['src/index.html', 'src/**/*.js']).on('change', browserSync.reload);
+  gulp.watch("src/*.pug", ['pug']);
+  gulp.watch(['dest/*.html', 'dest/js/**/*']).on('change', browserSync.reload);
 });
