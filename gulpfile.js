67aabc140872039b0bd2a5bd518f7a3a186b41d6
itklaus <it.thomas@yandex.ru>
2017-10-09 20:13:09 +0300

    add a sass

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,16 @@ const browserSync = require('browser-sync').create();
 const rename = require('gulp-rename');
 const pug = require('gulp-pug');
 const notify = require("gulp-notify");
+const sass = require('gulp-sass'); 
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
 
 gulp.task('es6', () => {
   return gulp.src('src/js/es6.js')
     .pipe(babel().on("error", notify.onError()))
     .pipe(rename('../dist/js/master.js'))
-    .pipe(gulp.dest('src'));
+    .pipe(gulp.dest('src'))
+    .pipe(browserSync.stream());
 });
 
 gulp.task('pug', () => {
@@ -20,7 +24,17 @@ gulp.task('pug', () => {
   .pipe(gulp.dest('dist/'))
 });
 
-gulp.task('default', ['es6', 'pug'], () => {
+gulp.task('sass', function () {
+  return gulp.src('src/sass/**/*.sass')
+    .pipe(sass().on("error", notify.onError()))
+    .pipe(autoprefixer(['last 14 versions']))
+    .pipe(cleanCSS()) 
+    .pipe(rename({suffix: '.min', prefix : ''}))
+    .pipe(gulp.dest('dist/css'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('default', ['es6', 'pug', 'sass'], () => {
   browserSync.init({
     server: {
       baseDir: 'dist'
@@ -28,6 +42,7 @@ gulp.task('default', ['es6', 'pug'], () => {
     notify: false
   });
   gulp.watch('src/**/*.js', ['es6']);
+  gulp.watch('src/sass/master.sass', ['sass']);
   gulp.watch("src/*.pug", ['pug']);
-  gulp.watch(['dist/*.html', 'dist/js/**/*']).on('change', browserSync.reload);
+  gulp.watch('dist/*.html').on('change', browserSync.reload);
 });
